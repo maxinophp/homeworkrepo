7a82a12820394cbadf3b1ef7083b79188102567d
maxinophp <malkhazi.tevzadze05@geolab.edu.ge>
2017-05-13 22:03:48 +0400

    php code changed

--- code.php
+++ code.php
@@ -8,26 +8,14 @@
         Firstname:<br> <input type="text" name="Firstname" value=""><br>
         Lastname:<br> <input type="text" name="Lastname" value=""><br>
         Gender: <br>
-        <select name="Gender" id="Gender">
+        <select name="Gender">
+        <option selected disabled></option>
         <option>Male</option>
         <option>Female</option></select><br>
-        Date:<br> <input type="text" id="datepicker" name="datepicker"><br>
+        Date:<br> <input type="Date" name="datepicker"><br>
         Short Biography: <br><textarea type="text" name="textarea"></textarea><br>
         Select checkbox: <input type="checkbox" name="checkbox"><br>
         <button type="Submit">Submit</button>
     </form>
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <link rel="stylesheet" href="/resources/demos/style.css">
-    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    <script>
-    $( function() {
-        $( "#datepicker" ).datepicker();
-    } );
-    </script>
-    <script>
-    $( function() {
-        $( "#Gender" ).selectmenu();
-    </script>
 </body>
 </html>
--- submit.php
+++ submit.php
@@ -1,12 +1,14 @@
 <?php
-    if ( $_SERVER ['REQUEST_METHOD'] == 'POST')    {
-     
-     echo $_POST['Firstname']."<br>";
-     echo $_POST['Lastname']."<br>";
-     echo $_POST['Gender']."<br>";
-     echo $_POST['datepicker']."<br>";
-     echo $_POST['textarea']."<br>";
-     echo $_POST['checkbox']."<br>";
- }
+  if ( $_SERVER ['REQUEST_METHOD'] == 'POST')    {
+     if (!empty($_POST['Firstname']) && !empty($_POST['Lastname']) && isset($_POST['Gender']) && !empty($_POST['datepicker']) && !empty($_POST['textarea']) && isset($_POST['checkbox'])) {
+     	echo $_POST['Firstname']."<br>";
+     	echo $_POST['Lastname']."<br>";
+     	echo $_POST['Gender']."<br>";
+          echo $_POST['datepicker']."<br>";
+          echo $_POST['textarea']."<br>";
+          echo $_POST['checkbox']."<br>"; 
+     }else {
+          header("Location: code.php");
+     }
+}
 ?>
-
